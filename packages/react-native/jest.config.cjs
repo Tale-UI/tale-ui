9051edcd3eacda8c42c81cf125627ea6a2d53285
Andrew <74314984+ndrewtran@users.noreply.github.com>
2026-07-30 01:11:05 +1000

    test: resolve native tokens in React Native Jest

--- packages/react-native/jest.config.cjs
+++ packages/react-native/jest.config.cjs
@@ -2,6 +2,9 @@ module.exports = {
   preset: '@react-native/jest-preset',
   watchman: false,
   modulePathIgnorePatterns: ['<rootDir>/build/'],
+  moduleNameMapper: {
+    '^@tale-ui/tokens/native$': '<rootDir>/../tokens/src/generated.ts',
+  },
   testMatch: ['<rootDir>/test/**/*.test.ts?(x)'],
   transformIgnorePatterns: [
     'node_modules/.pnpm/(?!(?:@react-native\\+|react-native@|@testing-library\\+react-native@))',
